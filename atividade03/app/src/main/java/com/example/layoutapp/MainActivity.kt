c64c9b8313c39c59c880c5ccf552bd231994430b
Fernando Lima <eufernandoprogramador@gmail.com>
2024-11-12 14:20:53 -0300

    Resolução da Atividade 03 Menu três pontinhos

---
+++ atividade03/app/src/main/java/com/example/layoutapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.layoutapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    // Infla o menu na ActionBar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // Lida com cliques nos itens do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, "Configurações selecionadas", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_help -> {
+                Toast.makeText(this, "Ajuda selecionada", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_about -> {
+                Toast.makeText(this, "Sobre selecionado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
